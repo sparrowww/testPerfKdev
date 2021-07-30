5acb9aa23a01a17cd03fda32c0d7cea792c3dced
sparrowww <sparrow-al@mail.ru>
2021-07-30 16:38:27 +0300

    thread static test

--- include/testFile3.hpp
+++ include/testFile3.hpp
@@ -5,7 +5,13 @@
 #include "unistd.h"
 #include "pthread.h"
 
+#include <chrono>
+#include <ctime>
+#include <ratio>
+
 using namespace std;
+
+using namespace chrono;
 
 #define msleep(us) usleep(us*1000)
 
@@ -18,8 +24,16 @@ class CMyThread
       void myPollingFuncThread();
       void startPthread();
    private:
-      pthread_t  myPthread;
       void* ( *pthreadFuncName ) ( void* );
+      inline void startChronoTime ();
+      inline double stopChronoTime ( const string &text );
+      unsigned short crc16Static ( unsigned short &data );
+      unsigned short crc16NoStatic ( unsigned short &data );
+   private:
+      pthread_t  myPthread;
+      high_resolution_clock::time_point difft1; ///< начальное время при запуске таймера
+      high_resolution_clock::time_point difft2; ///< конечное время при остановке таймера
+      duration<double, milli> time_span;
 };
 
 #endif
